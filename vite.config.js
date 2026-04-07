587994c688afc71ba322194fa63a570c7e7416df
Aleksandr <aleksandr_ermakov@outlook.com>
2026-04-07 23:55:39 +0300

    readme

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import babel from "@rolldown/plugin-babel";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-  base: "result-react-server-requests-task1",
+  // base: "/result-react-server-requests-task1/",
+  base: "/result-react-forms-task0/",
   server: {
     open: true,
   },
